21a5a5385167d234b207468482d88cbc20edc1d6
github_SONIA1612 <samarisonia@gmail.com>
2024-07-10 21:35:07 +0100

    Checkout Page

--- src/Components/Pages/Checkout.jsx
+++ src/Components/Pages/Checkout.jsx
@@ -2,7 +2,7 @@
 function Checkout() {
     return (
         <>
-            <section>
+            <section className=" Checkout-Main-section">
                 <div className="Checkout-h1">
                     <h1>Checkout</h1>
                 </div>
@@ -44,53 +44,54 @@ function Checkout() {
                                     <input className="Office-address" type="text" name="" placeholder=" " />
                                 </div>
                             </div>
+                        </form>
 
-                            <div className="delivery-method-container">
-                                <h3>Delivery method</h3>
+                        <div className="delivery-method-container">
+                            <h3>Delivery method</h3>
 
-                                <div>
-                                    <label>Express delivery</label>
-                                    <input type="text" name="" placeholder="Express delivery (1-2 days) $15 " />
+                            <div>
+                                <label>Express delivery</label>
+                                <input type="text" name="" placeholder="Express delivery (1-2 days) $15 " />
+                            </div>
+
+                            <div>
+                                <label>Standard delivery</label>
+                                <input type="text" name="" placeholder="Standard delivery (5-7 days)  " />
+                            </div>
+                        </div>
+
+
+
+                        {/* <div className="Shipping-details">
+                            <h3>Shipping-details</h3>
+
+                            <div className="Contact-detail">
+                                <div className="First-Name-container">
+                                    <label className="First-Name-Label">Country</label>
+                                    <input className="First-Name" type="text" name="" placeholder=" " />
                                 </div>
 
-                                <div>
-                                    <label>Standard delivery</label>
-                                    <input type="text" name="" placeholder="Standard delivery (5-7 days)  " />
+                                <div className="Email-container">
+                                    <label className="Address">Address</label>
+                                    <input className="Email-address" type="text" name="" placeholder=" " />
                                 </div>
                             </div>
 
-
-
-                            <div className="Shipping-details">
-                                <h3>Shipping-details</h3>
-
-                                <div className="Contact-detail">
-                                    <div className="First-Name-container">
-                                        <label className="First-Name-Label">Country</label>
-                                        <input className="First-Name" type="text" name="" placeholder=" " />
-                                    </div>
-
-                                    <div className="Email-container">
-                                        <label className="Address">Address</label>
-                                        <input className="Email-address" type="text" name="" placeholder=" " />
-                                    </div>
+                            <div className="Contact-detail-2">
+                                <div className="Last-Name-Container">
+                                    <label className="Last-Name-Label">Postal Code</label>
+                                    <input className="last-Name" type="text" name="" placeholder=" " />
                                 </div>
 
-                                <div className="Contact-detail-2">
-                                    <div className="Last-Name-Container">
-                                        <label className="Last-Name-Label">Postal Code</label>
-                                        <input className="last-Name" type="text" name="" placeholder=" " />
-                                    </div>
-
-                                    <div className="First-Name-container">
-                                        <label className="Phone-number-Label">City</label>
-                                        <input className="phone-number" type="text" name="" placeholder=" " />
-                                    </div>
+                                <div className="First-Name-container">
+                                    <label className="Phone-number-Label">City</label>
+                                    <input className="phone-number" type="text" name="" placeholder=" " />
                                 </div>
                             </div>
+                        </div> */}
 
 
-
+                        {/*
                             <div className="payment-details">
                                 <h3>Payment details</h3>
                                 <div>
@@ -133,9 +134,102 @@ function Checkout() {
                                 </div>
                             </div>
 
-                        </form>
+                        </form>*/}
                     </div>
-                    <div className="Cart-summary-2"></div>
+                    <div className="Cart-summary-2">
+                        <div className="cart-main-container">
+                            <div className="cart-summary-2">
+                                {/* <div className="cart-sub-container-1"> */}
+                                <div className="Zaraco-container-2">
+                                    <div className="img-2">
+                                        <img src="../images/yellow bag.jpg" alt="" />
+
+                                    </div>
+                                    <div className="yellow-bag-description2">
+                                        <h3 className="h3-2">Zaroca</h3>
+                                        <p className="Art-2">Art:234579</p>
+                                        <p className="Colour2">Colour: lemon green</p>
+                                        <p className="Quality2">Quality: premium luxuary</p>
+                                    </div>
+
+                                    <div className="price-container-2">
+                                        <p className="price-2">$230.00</p>
+                                    </div>
+                                </div>
+                                <div className="remove-container-2">
+                                    <div className="add-con">
+                                        <p className="sub"> - </p>
+                                        <p className="num-1">1</p>
+                                        <p classname="add"> + </p>
+                                    </div>
+
+                                    <div className="Remove-2">
+                                        <h2>Remove</h2>
+                                    </div>
+                                </div>
+                                {/* </div> */}
+                            </div>
+                            <div className="cart-summary-3">
+                                <div className="Zaraco-container-2">
+                                    <div className="img-2">
+                                        <img src="../images/yellow bag.jpg" alt="" />
+
+                                    </div>
+                                    <div className="yellow-bag-description2">
+                                        <h3 className="h3-2">Zaroca</h3>
+                                        <p className="Art-2">Art:234579</p>
+                                        <p className="Colour2">Colour: lemon green</p>
+                                        <p className="Quality2">Quality: premium luxuary</p>
+                                    </div>
+
+                                    <div className="price-container-2">
+                                        <p className="price-2">$230.00</p>
+                                    </div>
+                                </div>
+                                <div className="remove-container-2">
+                                    <div className="add-con">
+                                        <p className="sub"> - </p>
+                                        <p className="num-1">1</p>
+                                        <p classname="add"> + </p>
+                                    </div>
+
+                                    <div className="Remove-2">
+                                        <h2>Remove</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cart-summary-4">
+                                <div className="Zaraco-container-2">
+                                    <div className="img-2">
+                                        <img src="../images/yellow bag.jpg" alt="" />
+
+                                    </div>
+                                    <div className="yellow-bag-description2">
+                                        <h3 className="h3-2">Zaroca</h3>
+                                        <p className="Art-2">Art:234579</p>
+                                        <p className="Colour2">Colour: lemon green</p>
+                                        <p className="Quality2">Quality: premium luxuary</p>
+                                    </div>
+
+                                    <div className="price-container-2">
+                                        <p className="price-2">$230.00</p>
+                                    </div>
+                                </div>
+                                <div className="remove-container-2">
+                                    <div className="add-con">
+                                        <p className="sub"> - </p>
+                                        <p className="num-1">1</p>
+                                        <p classname="add"> + </p>
+                                    </div>
+
+                                    <div className="Remove-2">
+                                        <h2>Remove</h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="cart-summary-5"></div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
